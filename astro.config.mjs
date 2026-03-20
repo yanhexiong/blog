195a5ccfaeaf3d8388c8dcc958fcf6b4b9021ec5
YHX <592604831@qq.com>
2026-03-20 21:29:37 +0800

    modify domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.727613.xyz/",
+	site: "https://blog.727613.xyz",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
